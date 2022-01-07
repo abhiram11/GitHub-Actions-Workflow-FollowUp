b676a01435059b0f8e3be57e2b31b4b03af4f1fb
abhiram11 <abhiramsatpute@gmail.com>
2022-01-07 16:35:34 +0530

    update code, removed tree command from yaml

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hiiiiiiiii test app deploy from abhiram! </h1>
+      <h2>New line added to validate the bug fix!</h2>
     </div>
   );
 }
